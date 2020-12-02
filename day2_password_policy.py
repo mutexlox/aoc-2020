71eefadd8c9200884147addb69c7def8678fa616
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2020-12-02 07:55:18 -0800

    Simplify day 2 code

--- day2_password_policy.py
+++ day2_password_policy.py
@@ -1,31 +1,27 @@
 import sys
 
-def count_valid_pt1(lines):
-    valid = 0
-    for l in lines:
-        parts = l.split()
-        lo, hi = parts[0].split('-')
-        cnt = parts[2].count(parts[1][:-1])
-        if cnt >= int(lo) and cnt <= int(hi):
-            valid += 1
-    return valid
+def validate_pt1(lo, hi, char, pw):
+    cnt = pw.count(char)
+    return cnt >= lo and cnt <= hi
 
-def count_valid_pt2(lines):
+def validate_pt2(lo, hi, char, pw):
+    return (pw[lo - 1] == char) != (pw[hi - 1] == char)
+
+
+def count_valid(lines, fn):
     valid = 0
     for l in lines:
         parts = l.split()
         lo, hi = parts[0].split('-')
-        lo, hi = int(lo), int(hi)
-        want = parts[1][:-1]
-        if (parts[2][lo - 1] == want) != (parts[2][hi - 1] == want):
+        if fn(int(lo), int(hi), parts[1][:-1], parts[2]):
             valid += 1
     return valid
 
 def main(argv):
     with open(argv[1]) as f:
         lines = [x.rstrip() for x in f]
-        print(count_valid_pt1(lines))
-        print(count_valid_pt2(lines))
+        print(count_valid(lines, validate_pt1))
+        print(count_valid(lines, validate_pt2))
 
 if __name__ == "__main__":
     main(sys.argv)
