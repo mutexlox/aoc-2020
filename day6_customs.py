7424f00df911c96bd0b06d496b1421a69ecd90de
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2020-12-05 21:13:14 -0800

    Simplify day 6 impl.

--- day6_customs.py
+++ day6_customs.py
@@ -2,33 +2,25 @@ import sys
 
 def count_yesses(lines):
     group = []
+    group_size = 0
+
     count = 0
+    count_all_yes = 0
+
     for l in lines:
         if not l:
             s = set(group)
             count += len(s)
-            group = []
-        else:
-            group.extend(l)
-
-    return count
-
-def count_all_yesses(lines):
-    group = []
-    group_size = 0
-    count = 0
-    for l in lines:
-        if not l:
-            for c in set(group):
+            for c in s:
                 if group.count(c) == group_size:
-                    count += 1
+                    count_all_yes += 1
             group = []
             group_size = 0
         else:
             group.extend(l)
             group_size += 1
 
-    return count
+    return count, count_all_yes
 
 
 def main(argv):
@@ -36,7 +28,6 @@ def main(argv):
         lines = [x.rstrip() for x in f]
         lines.append('')
         print(count_yesses(lines))
-        print(count_all_yesses(lines))
 
 
 if __name__ == "__main__":
