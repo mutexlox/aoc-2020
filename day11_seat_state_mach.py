42b920e08d902930a30ae862a04e237789b3060e
Miriam Zimmerman <876194+mutexlox@users.noreply.github.com>
2020-12-13 13:30:00 -0800

    further day11 cleanup

--- day11_seat_state_mach.py
+++ day11_seat_state_mach.py
@@ -3,8 +3,6 @@ import copy
 import sys
 
 SCRATCH = None
-ROWS = 0
-COLS = 0
 
 def copy_state(src, dest):
     for i in range(len(src)):
@@ -20,8 +18,8 @@ def get_visible(state, row, col, keep_going):
                 if i == 0 and j == 0:
                     continue
                 k = 1
-                while (row + i * k >= 0 and row + i * k < ROWS and
-                    col + j * k >= 0 and col + j * k < COLS):
+                while (row + i * k >= 0 and row + i * k < len(state) and
+                    col + j * k >= 0 and col + j * k < len(state[row])):
                     if state[row + i * k][col + j * k] != '.':
                         VISIBLE_SEATS[(row,col)].append((row + i * k, col + j * k))
                         break
@@ -31,58 +29,42 @@ def get_visible(state, row, col, keep_going):
     return VISIBLE_SEATS[(row, col)]
 
 
-def step(state):
-    for row in range(ROWS):
-        for col in range(COLS):
+def step(state, keep_going, max_num_occ):
+    for row in range(len(state)):
+        for col in range(len(state[row])):
             if state[row][col] == '.':
                 continue
             occ = 0
-            for new_i, new_j in get_visible(state, row, col, False):
+            for new_i, new_j in get_visible(state, row, col, keep_going):
                 if state[new_i][new_j] == '#':
                     occ += 1
             if occ == 0 and state[row][col] == 'L':
                 SCRATCH[row][col] = '#'
-            elif occ >= 4 and state[row][col] == '#':
+            elif occ >= max_num_occ and state[row][col] == '#':
                 SCRATCH[row][col] = 'L'
 
 
-def step_pt2(state):
-    for row in range(ROWS):
-        for col in range(COLS):
-            if state[row][col] == '.':
-                continue
-            occ = 0
-            for new_i, new_j in get_visible(state, row, col, True):
-                if state[new_i][new_j] == '#':
-                    occ += 1
-            if occ == 0 and state[row][col] == 'L':
-                SCRATCH[row][col] = '#'
-            elif occ >= 5 and state[row][col] == '#':
-                SCRATCH[row][col] = 'L'
-
-
-def iterate(init, f):
+def iterate(init, keep_going, max_num_occ):
     global VISIBLE_SEATS
-    state = init
     VISIBLE_SEATS = collections.defaultdict(list)
+
+    state = init
     copy_state(state, SCRATCH)
-    f(state)
+    step(state, keep_going, max_num_occ)
     while state != SCRATCH:
         copy_state(SCRATCH, state)
-        f(state)
+        step(state, keep_going, max_num_occ)
     return sum([r.count('#') for r in state])
 
 
 def main(argv):
-    global SCRATCH, ROWS, COLS
+    global SCRATCH
     with open(argv[1]) as f:
         lines = [[c for c in x.rstrip()] for x in f]
-        ROWS = len(lines)
-        COLS = len(lines[0])
         SCRATCH = copy.deepcopy(lines)
         state = copy.deepcopy(lines)
-        print(iterate(state, step))
-        print(iterate(lines, step_pt2))
+        print(iterate(state, False, 4))
+        print(iterate(lines, True, 5))
 
 
 if __name__ == "__main__":
